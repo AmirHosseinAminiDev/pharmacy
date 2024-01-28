6aa8acf174d2b6e27a22aba7542c1c70aa807b16
AmirHosseinAmini <amirhosseinaminidev@gmail.com>
2024-01-28 20:30:06 +0330

    created app first page and added HomeController into User directory and also added web routes

---
+++ app/Http/Controllers/User/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * @return Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+     */
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    {
+        return view('user.index');
+    }
+}
